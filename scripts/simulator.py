3d120789ff53dbf2ca957042292537f5732d5897
Christian Hellum Bye <chbye@berkeley.edu>
2023-12-13 20:36:46 -0800

    properly compute ratio of integration times

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -11,7 +11,7 @@ TRUE_PARAMS = {"a": -0.2, "w": 20, "nu21": 80}
 BOUNDS = np.array([[-1.0, 1.0], [1.0, 60.0], [45.0, 105.0]])
 NDIM = len(BOUNDS)
 N_PARTICLES = 1000
-NBINS = 8
+NBINS = 24
 NFG = np.arange(4, 9)
 
 lst, freq, temp = utils.read_hdf5_convolution(
@@ -23,15 +23,15 @@ temp = temp[:, indx]
 nspec, nfreq = temp.shape
 fg_mean = temp.mean(axis=0)  # avg spectrum, fg only
 # this discards the last 6 min integration
-fg_bin = (
-    temp[: -(nspec % NBINS)].reshape(nspec // NBINS, NBINS, nfreq).mean(axis=0)
-)
+fg_bin = temp[: -(nspec % NBINS)].reshape(nspec // NBINS, NBINS, nfreq)
+tint_ratio = fg_bin.shape[0] / nspec
+fg_bin = fg_bin.mean(axis=0)
 
 # noise
 noise_75 = 3e-3
 t75 = fg_mean[freq == 75]
 noise, noise_cov_inv = utils.gen_noise(
-    fg_bin, t75, ref_noise=noise_75, tint_ratio=1 / NBINS
+    fg_bin, t75, ref_noise=noise_75, tint_ratio=tint_ratio
 )
 
 # injected 21cm signal
@@ -41,16 +41,17 @@ true_t21 = utils.gauss(freq, **TRUE_PARAMS)
 sampler = Sampler(N_PARTICLES, NDIM, BOUNDS, n_cpus=N_CPUS)
 results = {}
 for i in range(NBINS):
-    print(f"Bin {i+1}/{NBINS}")
     results[i] = {}
     for n in NFG:
-        print(f"NFG = {n}")
-        lst_bin = LSTBin(freq, fg_bin[i], noise_cov_inv[i], true_t21, n)
+        print(f"Bin {i+1}/{NBINS}, NFG = {n}/{NFG[-1]}")
+        lst_bin = LSTBin(
+            freq, fg_bin[i]+noise[i], np.diag(noise_cov_inv[i]), true_t21, n
+        )
         results[i][n] = sampler.run_sampler(lst_bin)
 
 # save the results
 np.savez(
-    "results.npz",
+    "results_dec13_24bins.npz",
     results=results,
     true_params=TRUE_PARAMS,
     noise=noise,
