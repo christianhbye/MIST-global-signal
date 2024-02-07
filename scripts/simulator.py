4d88e925e33acd5c6de90c65a543cd90a30f4eba
Christian Hellum Bye <chbye@berkeley.edu>
2024-02-07 15:36:08 -0800

    add parallelization

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing import Pool
 import numpy as np
 from mistsim import LSTBin, Sampler, utils
 
@@ -10,7 +11,7 @@ TRUE_PARAMS = {"a": -0.2, "w": 20, "nu21": 80}
 # Parameter bounds (a, w, nu21)
 BOUNDS = np.array([[-1.0, 1.0], [1.0, 60.0], [45.0, 105.0]])
 NDIM = len(BOUNDS)
-N_PARTICLES = 1000
+N_PARTICLES = 64
 NBINS = 24
 NFG = np.arange(4, 9)
 
@@ -21,37 +22,49 @@ indx = (freq >= 45) * (freq <= 105)
 freq = freq[indx]
 temp = temp[:, indx]
 nspec, nfreq = temp.shape
-fg_mean = temp.mean(axis=0)  # avg spectrum, fg only
-# this discards the last 6 min integration
-fg_bin = temp[: -(nspec % NBINS)].reshape(nspec // NBINS, NBINS, nfreq)
-tint_ratio = fg_bin.shape[0] / nspec
-fg_bin = fg_bin.mean(axis=0)
 
-# noise
-noise_75 = 3e-3
-t75 = fg_mean[freq == 75]
-noise, noise_cov_inv = utils.gen_noise(
-    fg_bin, t75, ref_noise=noise_75, tint_ratio=tint_ratio
-)
 
 # injected 21cm signal
 true_t21 = utils.gauss(freq, **TRUE_PARAMS)
 
+
+# binning
+cut = nspec % NBINS
+total_temp = temp + true_t21
+
+if cut == 0:
+    binned = total_temp.copy()
+else:
+    binned = total_temp[:-cut]
+binned = binned.reshape(nspec//NBINS, NBINS, nfreq).mean(axis=0)
+
+# noise
+tint_ratio = (nspec//NBINS) / nspec
+noise_75 = 3e-3
+t75 = total_temp.mean(axis=0)[freq == 75]
+noise, sigma_inv = utils.gen_noise(
+    binned, t75, ref_noise=noise_75, tint_ratio=tint_ratio
+)
+
+
 # initialize the sampler and run the simulations
-sampler = Sampler(N_PARTICLES, NDIM, BOUNDS, n_cpus=N_CPUS)
-results = {}
-for i in range(NBINS):
-    results[i] = {}
+sampler = Sampler(N_PARTICLES, NDIM, BOUNDS, seed=0)
+
+def _loop(i):
+    d = {}
     for n in NFG:
-        print(f"Bin {i+1}/{NBINS}, NFG = {n}/{NFG[-1]}")
-        lst_bin = LSTBin(
-            freq, fg_bin[i]+noise[i], np.diag(noise_cov_inv[i]), true_t21, n
-        )
-        results[i][n] = sampler.run_sampler(lst_bin)
+        print(f"{i=}: {n}/{NFG}")
+        lst_bin = LSTBin(freq, binned[i]+noise[i], np.diag(sigma_inv[i]), n)
+        d[n] = sampler.run_sampler(lst_bin, add_samples=int(1e4))
+    return d
+
+pool = Pool(N_CPUS)
+results = zip(*pool.map(_loop, range(NBINS)))
+results = {i : results[i] for i in range(len(results))}
 
 # save the results
 np.savez(
-    "results_dec13_24bins.npz",
+    "results_feb7_24bins.npz",
     results=results,
     true_params=TRUE_PARAMS,
     noise=noise,
