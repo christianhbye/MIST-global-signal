af54325235e5fb5391e113ea03169f78909a281f
Christian Hellum Bye <chbye@berkeley.edu>
2024-02-23 09:42:26 -0800

    allow kwargs passed to pc.Sampler.init

--- mistsim/sampler.py
+++ mistsim/sampler.py
@@ -40,22 +40,38 @@ def log_likelihood(params, lst_bins):
     return lnL
 
 
-def run_sampler(bounds, lst_bin, vectorize=True, pool=None, **kwargs):
+def uniform_prior(bounds):
+    """
+    A uniform prior distribution.
+
+    Parameters
+    ----------
+    bounds : np.ndarray
+        The bounds of the parameter space. Shape (ndim, 2).
+
+    Returns
+    -------
+    prior : pc.Prior
+        A uniform prior distribution.
+    """
+    loc = bounds.T[0]
+    scale = bounds.T[1] - bounds.T[0]
+    return pc.Prior([uniform(loc=lo, scale=s) for lo, s in zip(loc, scale)])
+
+
+def run_sampler(prior, lst_bin, progress=True, **kwargs):
     """
     Run the sampler.
 
     Parameters
     ----------
-    bounds : ndarray
-        The bounds of the parameter space for the prior distribution.
+    prior : callable
+        The prior distribution.
     lst_bin : LSTBin or list of LSTBin
-    vectorize : bool
-        Make a vectorized call to the likelihood function.
-    pool : Pool
-        A pool of workers for parallel evaluation of the likelihood. Does not
-        seen to have an effect if vectorize is True.
-    **kwargs : dict
-        Passed to `pc.Sampler.run`.
+    progress : bool
+        Display a progress bar.
+    kwargs : dict
+        Additional keyword arguments passed to pc.Sampler.
 
     Returns
     -------
@@ -71,22 +87,18 @@ def run_sampler(bounds, lst_bin, vectorize=True, pool=None, **kwargs):
     """
     if isinstance(lst_bin, LSTBin):
         lst_bin = [lst_bin]
-    loc = bounds.T[0]
-    scale = bounds.T[1] - bounds.T[0]
-    prior = pc.Prior([uniform(loc=lo, scale=s) for lo, s in zip(loc, scale)])
-    ndim = len(bounds)
-    args = (prior, log_likelihood, ndim)
     sampler = pc.Sampler(
-        *args, likelihood_args=[lst_bin], vectorize=vectorize, pool=pool
+        prior, log_likelihood, likelihood_args=[lst_bin], **kwargs
     )
-    sampler.run(**kwargs)
+    sampler.run(progress=progress)
 
     results = {}
     samples = sampler.posterior(resample=True)[0]
     results["samples"] = samples
     theta_map = np.mean(samples, axis=0)
     lnL = log_likelihood(theta_map, lst_bin)
-    nparams = ndim + np.sum([spec.nfg for spec in lst_bin])
+    # 3 parameters for the Gaussian + 1 for each foreground component
+    nparams = 3 + np.sum([spec.nfg for spec in lst_bin])
     nfreq = lst_bin[0].freq.size
     bic = -2 * lnL + nparams * np.log(nfreq)
     results["theta_map"] = theta_map
